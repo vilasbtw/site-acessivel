8a049dd0a1adf4ad267afecf5f17132801666d84
andineto <122061209+andineto@users.noreply.github.com>
2024-06-11 21:28:46 -0300

    add mascara no formulário telefone

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var fontSizeBig = false;
 const body = document.body;
 var footer = document.getElementsByClassName("rodape");
 var textElements = document.querySelectorAll("body, body *:not(script):not(style):not(header ul):not(iframe)");
+var telInput = document.getElementById('tel');
+
 
 
 function darkMode() {
@@ -11,8 +13,7 @@ function darkMode() {
 
   // DEIXA A IMAGEM COM FUNDO BRANCO PARA CONTINUAR APARECENDO
   document.getElementById("menuImg").style.backgroundColor = "white";
-  
-  footer[0].style.backgroundColor = "black";
+
   footer[0].style.color = "yellow";
   
   darkModeOn = true;
@@ -21,7 +22,6 @@ function darkMode() {
 function lightMode() {
   body.style.backgroundColor = "";
   body.style.color = "";
-  footer[0].style.backgroundColor = "";
   footer[0].style.color = "";
   darkModeOn = false;
 }
@@ -55,6 +55,54 @@ function toggleFontSize(){
     increaseFontSize();
   }
   }
+
+
+  function formataCampo(campo, Mascara, evento) { 
+    var boleanoMascara; 
+    
+    var Digitato = evento.keyCode;
+    exp = /\-|\.|\/|\(|\)| /g
+    campoSoNumeros = campo.value.toString().replace( exp, "" ); 
+
+    var posicaoCampo = 0;    
+    var NovoValorCampo="";
+    var TamanhoMascara = campoSoNumeros.length;; 
+    
+    if (Digitato != 8) {
+            for(i=0; i<= TamanhoMascara; i++) { 
+                    boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
+                                                            || (Mascara.charAt(i) == "/")) 
+                    boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") 
+                                                            || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " ")) 
+                    if (boleanoMascara) { 
+                            NovoValorCampo += Mascara.charAt(i); 
+                              TamanhoMascara++;
+                    }else { 
+                            NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
+                            posicaoCampo++; 
+                      }              
+              }      
+            campo.value = NovoValorCampo;
+              return true; 
+    }else { 
+            return true; 
+    }
+  }
+
+    function mascaraInteiro(){
+      if (event.keyCode < 48 || event.keyCode > 57){
+              event.returnValue = false;
+              return false;
+      }
+      return true;
+}
+
+function mascaraPhone(editPhone){
+  if(mascaraInteiro(editPhone)==false){
+  event.returnValue = false;
+}       
+return formataCampo(editPhone, '(00)0000-0000', event);
+}
 
 
 document.getElementById("ac").addEventListener("click", toggleDarkMode);
